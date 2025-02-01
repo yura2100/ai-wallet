89f9904e8c7d8903b6077a2b147e82f068b98521
ikeretsman <illia.k@devbrother.com>
2025-02-01 23:29:10 +0200

    implemented okx api client

---
+++ lib/okx-client/get-total-balance-usd.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { createResponseSchema, getHeaders, getUrl } from "./utils";
+
+const getTotalBalanceUsdSchema = createResponseSchema(
+  z.object({ totalValue: z.string().transform((value) => Number(value)) })
+);
+
+export async function getTotalBalanceUsd(address: string, chain: number) {
+  try {
+    const params = new URLSearchParams();
+    params.append("address", address);
+    params.append("chains", chain.toString());
+
+    const { endpointWithParams, requestUrl } = getUrl(
+      "getTotalBalanceUsd",
+      params
+    );
+    const method = "GET";
+    const headers = getHeaders(endpointWithParams, method);
+
+    const response = await fetch(requestUrl, { headers, method });
+    const responseBody = await response.json();
+
+    const { data } = getTotalBalanceUsdSchema.parse(responseBody);
+
+    console.log(data);
+
+    return {
+      totalBalanceUsd: data[0]?.totalValue ?? 0,
+    };
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
---
+++ lib/okx-client/index.ts
@@ -0,0 +1,2 @@
+export * from "./get-all-token-balances";
+export * from "./get-transactions";
